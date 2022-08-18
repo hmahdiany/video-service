698865e23983c83b2ad7aea21516d9b7e44d9801
Hossein Mahdiany <hossein.mahdiany@gmail.com>
2022-08-18 21:46:45 +0430

    customize logging and adding validation

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"gin/controller"
+	"gin/middlewares"
 	"gin/service"
+	"io"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -12,15 +17,32 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setLogOutput() {
+	f, _ := os.Create("/tmp/gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+	// log output
+	setLogOutput()
+
+	server := gin.New()
+
+	// use middleware
+	server.Use(gin.Recovery(), middlewares.Logger(),
+		middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
 
 	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
+		err := videoController.Save(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid."})
+		}
 	})
 
 	server.Run(":5000")
