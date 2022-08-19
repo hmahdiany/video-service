321fe0e41e74b1680fe55609764f7801120f6163
Hossein Mahdiany <hossein.mahdiany@gmail.com>
2022-08-19 12:54:27 +0430

    serve static files

--- main.go
+++ main.go
@@ -28,22 +28,36 @@ func main() {
 
 	server := gin.New()
 
+	// serve static files
+	server.Static("css", "templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
+
 	// use middleware
 	server.Use(gin.Recovery(), middlewares.Logger(),
 		middlewares.BasicAuth(), gindump.Dump())
 
-	server.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.FindAll())
-	})
+	// create api group
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
 
-	server.POST("/videos", func(ctx *gin.Context) {
-		err := videoController.Save(ctx)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		} else {
-			ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid."})
-		}
-	})
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid."})
+			}
+		})
+	}
 
+	// create view group
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
 	server.Run(":5000")
 }
